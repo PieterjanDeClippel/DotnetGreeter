503f0266925dc66b3f1f14c265795dc88b385f3d
PieterjanDeClippel <pieterjandeclippel@msn.com>
2025-11-14 22:51:20 +0100

    Split files

---
+++ ConsoleApp4/Commands/Greet.Farewell.cs
@@ -0,0 +1,25 @@
+﻿using MintPlayer.CliGenerator.Attributes;
+
+namespace Demo;
+
+[CliCommand("farewell", Description = "Says farewell to a person")]
+[CliParentCommand(typeof(Greet))]
+public partial class Farewell : ICliCommand
+{
+    public Task<int> Execute(CancellationToken cancellationToken)
+    {
+        var farewellMessage = $"Goodbye, {Name}!";
+        if (!string.IsNullOrEmpty(MeetAgain))
+        {
+            farewellMessage += $" See you again {MeetAgain}.";
+        }
+        Console.WriteLine(farewellMessage);
+        return Task.FromResult(0);
+    }
+
+    [CliArgument(0, Name = "name", Description = "Name of the person to bid farewell")]
+    public string Name { get; set; }
+
+    [CliOption("--meet-again", "-m", Description = "When will we meet again")]
+    public string MeetAgain { get; set; }
+}
---
+++ ConsoleApp4/Commands/Greet.cs
@@ -0,0 +1,32 @@
+﻿using MintPlayer.CliGenerator.Attributes;
+
+namespace Demo;
+
+[CliParentCommand(typeof(GreetCommand))]
+[CliCommand("greet", Description = "Greets a person")]
+public partial class Greet : ICliCommand
+{
+    public Task<int> Execute(CancellationToken cancellationToken)
+    {
+        if (Verbose)
+            Console.WriteLine("Running demo command in verbose mode");
+
+        var message = $"Hello, {Name}!";
+        for (var i = 0; i < Times; i++)
+            Console.WriteLine(Uppercase ? message.ToUpperInvariant() : message);
+
+        return Task.FromResult(0);
+    }
+
+    [CliArgument(0, Name = "name", Description = "Name of the person to greet")]
+    public string Name { get; set; }
+
+    [CliArgument(1, Name = "times", Description = "Number of times to greet")]
+    public int Times { get; set; }
+
+    [CliOption("--uppercase", "-u", Description = "Output in uppercase")]
+    public bool Uppercase { get; set; }
+
+    [CliOption("--verbose", "-v", Description = "Enable verbose output")]
+    public bool Verbose { get; set; }
+}
